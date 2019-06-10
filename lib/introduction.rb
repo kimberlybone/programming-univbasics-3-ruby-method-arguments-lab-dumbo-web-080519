9ae50956344ea0ff572a9bf0f631b07fa56d04c1
Kimberly Bone <k_bone@uncg.edu>
2019-06-10 19:49:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,9 +10,8 @@ end
 introduction_with_language ("Dan", "Ember.js")
 introduction_with_language ("Josh", "React")
 
-language = "Ruby"
 def introduction_with_language_optional (name, language)
   puts "Hi, my name is #{name} and I am learning to pogram in #{language}"
 end 
-
-introduction_with_language_optional ("")
+language = "Ruby"
+introduction_with_language_optional ("Edwin", language)
